e9a6d29a2f02e67a6183567c87260c50bfe4fee2
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2020-01-11 22:06:17 -0500

    passed all test

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const inquirer = require('inquirer')
 
-inquirer.prompt([
-    {
-        name: 'username',
-        type: 'input',
-        message: 'what is your username'
-    }
-]).then(function(res){
-    console.log(res)
-}).catch(function(err){
-    if (err){
-        console.log(err);
-    }
-})
+        inquirer.prompt([
+            {
+                name: 'name',
+                type: 'input',
+                message: 'what is your name?'
+            },
+            {
+                name: 'email',
+                type: 'input',
+                message: 'what is your email?'
+            },
+            {
+                name: 'id',
+                type: 'list',
+                message: 'what is your id?',
+                choices:[1, 2, 3, 4, 5]
+            },
+            {
+                name: 'role',
+                type: 'input',
+                message: 'what is your role?'
+            },
+        
+        ]).then(function(res){
+            console.log(res)
+        }).catch(function(err){
+            if (err){
+                console.log(err);
+            }
+        })
+    
+
+
+
